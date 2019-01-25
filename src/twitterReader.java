63e1a91f6c42733c75ade5bb2003bc58efe2c563
svonhatten <svonhatten@luc.edu>
2019-01-25 16:45:25 -0600

    Added multiple URL finder, all methods have character limit ~SV

--- src/twitterReader.java
+++ src/twitterReader.java
@@ -1,36 +1,44 @@
-
 public class twitterReader {
 	
-	String msgstored;
-	
-	public twitterReader(String msg) {
-		msgstored = msg;
+	public twitterReader() {
 	}
 	
-	public String getmsg() {
-		return msgstored;
-	}
-	
-	public static String getURL(String testmsg) {
+	public static String[] getURLs(String testmsg) {
 		// regex expression for links found at https://stackoverflow.com/questions/163360/regular-expression-to-match-urls-in-java
 		String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
 
 		//splits message at whitespaces (space character " "), turning it into an array
 		String[] message = testmsg.split("\\s");
 
+		String[] URLList = new String[20];
+
+		//URL counter
+		int URLCount = 0;
+
+		//max number of characters in the twitter message declared
+		int maxLength = 280;
+		
+		//if message exceeds 280 characters, then return null...?
+		if(message.length > maxLength){
+			System.out.println("Error: character limit exceeded over 280 characters.");
+		}
+
 		//iterates through array of strings after they are split at whitespace characters (spacebar)
 		for(int i=0;i<message.length;i++){
+			
 			//if there is a period at the end of the string, it gets removed
 			if(message[i].charAt(message[i].length()-1) == '.'){
 				message[i] = message[i].substring(0,message[i].length()-1);
 			}
 			// if the word matches our regex to check for links(see above), return it
 			if(message[i].matches(regex)){
-				return message[i];
+				URLList[URLCount] = message[i];
+				URLCount++;
 			}
 		}
+		System.out.println(URLCount + "URLs found");
 		// otherwise return null to indicate no links were found
-		return null;
+		return URLList;
 	}
 	
 	public static String getTopic(String testmsg) {
@@ -39,6 +47,17 @@ public class twitterReader {
 
 		//splits message at whitespaces (space character " "), turning it into an array
 		String[] message = testmsg.split("\\s");
+
+		//URL counter
+		int URLcount = 0;
+
+		//max number of characters in the twitter message declared
+		int maxLength = 280;
+
+		//if message exceeds 280 characters, then return null...?
+		if(message.length > maxLength){
+			System.out.println("Error: character limit exceeded over 280 characters.");
+		}
 
 		//iterates through array of strings after they are split at whitespace characters (spacebar)
 		for(int i=0;i<message.length;i++){
@@ -62,6 +81,17 @@ public class twitterReader {
 		//splits message at whitespaces (space character " "), turning it into an array
 		String[] message = testmsg.split("\\s");
 
+		//URL counter
+		int URLcount = 0;
+
+		//max number of characters in the twitter message declared
+		int maxLength = 280;
+
+		//if message exceeds 280 characters, then return null...?
+		if(message.length > maxLength){
+			System.out.println("Error: character limit exceeded over 280 characters.");
+		}
+
 		//iterates through array of strings after they are split at whitespace characters (spacebar)
 		for(int i=0;i<message.length;i++){
 			//if there is a period at the end of the string, it gets removed
@@ -84,6 +114,17 @@ public class twitterReader {
 
 		//splits message at whitespaces (space character " "), turning it into an array
 		String[] message = testmsg.split("\\s");
+
+		//URL counter
+		int URLcount = 0;
+
+		//max number of characters in the twitter message declared
+		int maxLength = 280;
+
+		//if message exceeds 280 characters, then return null...?
+		if(message.length > maxLength){
+			System.out.println("Error: character limit exceeded over 280 characters.");
+		}
 
 		//iterates through array of strings after they are split at whitespace characters (spacebar)
 		for(int i = 0; i < message.length; i++) {
--- src/twitterReadertests.java
+++ src/twitterReadertests.java
@@ -4,37 +4,33 @@ class twitterReadertests {
 
 	public static void main(String [] args){
 		twitterReadertests test = new twitterReadertests();
-		System.out.println(test.testURL());
+		System.out.println(test.testURLs());
 		System.out.println(test.testTopic());
 		System.out.println(test.testMention());
 		System.out.println(test.testRetweet());
 	}
 
 
-	private boolean testURL() {
-		String testmsg = "@franky goes to #hollywood. See http://cnn.com.";
-		twitterReader test = new twitterReader(testmsg);
-		return("http://cnn.com".equals(twitterReader.getURL(testmsg)));
+	private boolean testURLs() {
+		String testmsg = "@franky goes to #hollywood. See http://cnn.com and http://abc.com";
+		System.out.println(twitterReader.getURLs(testmsg)[0] + " " + twitterReader.getURLs(testmsg)[1]);
+		return(twitterReader.getURLs(testmsg)[0].equals("http://cnn.com")&&twitterReader.getURLs(testmsg)[1].equals("http://abc.com"));
 	}
-
 
 	boolean testTopic() {
 		String testmsg = "@franky goes to #hollywood. See http://cnn.com.";
-		twitterReader test = new twitterReader(testmsg);
 		return("#hollywood".equals(twitterReader.getTopic(testmsg)));
 	}
 	
 
 	boolean testMention() {
 		String testmsg = "@franky goes to #hollywood. See http://cnn.com.";
-		twitterReader test = new twitterReader(testmsg);
 		return("@franky".equals(twitterReader.getMention(testmsg)));
 	}
 
 
 	boolean testRetweet() {
 		String testmsg = "RT @franky: cool story bro!";
-		twitterReader test = new twitterReader(testmsg);
 		System.out.println(twitterReader.getRetweet(testmsg));
 		return("RT @franky:".equals(twitterReader.getRetweet(testmsg)));
 		//retweeted_status
